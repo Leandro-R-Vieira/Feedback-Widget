032b5b0da4335a945993d7ebcbc0b8396da3ee79
Leandro Vieira <leandrorvieira22@yahoo.com>
2022-05-06 10:21:23 -0300

    fourth commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "47caf5393e1098",
+    pass: "7b10ef962ce3d7"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({subject, body}: SendMailData) {
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com',
+        to: 'Leandro Vieira <leandrorvieira@yahoo.com>',
+        subject,
+        html: body,
+      });
+  }; 
+}
